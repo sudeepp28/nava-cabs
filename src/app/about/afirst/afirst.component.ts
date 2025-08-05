f1fd113d61af4689d5c22b18e26d7cd03b1ea139
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-08-05 18:16:11 +0530

    cd

--- src/app/about/afirst/afirst.component.ts
+++ src/app/about/afirst/afirst.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-afirst',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './afirst.component.html',
   styleUrl: './afirst.component.css'
 })
-export class AfirstComponent {
+export class AfirstComponent implements OnInit, OnDestroy {
+  images: string[] = [
+    'assets/afirst.jpg',
+    'assets/asecond.jpg',
+    'assets/athird.jpg'
+  ];
+  currentIndex: number = 0;
+  currentImage: string = this.images[0];
+  intervalId: any;
 
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.currentImage = this.images[this.currentIndex];
+    }, 5000); // change image every 3 seconds
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
 }
